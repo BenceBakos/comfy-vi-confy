d68fb2b15d136e2c7e56a941fe8ca597955dfd35
bencebakos <bakos.bence.work@gmail.com>
2023-01-15 15:29:28 +0100

    no edit

--- init.lua
+++ init.lua
@@ -398,6 +398,3 @@ vim.api.nvim_create_autocmd(
 
 vim.opt.swapfile = false
 
--- edit file
-Keyboard.map('n','jk',':e<space>', { noremap = true, silent = false })
-Keyboard.map('n','kj',':e<space>', { noremap = true, silent = false })
