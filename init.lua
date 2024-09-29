f5b03b05dbe86a657e150143d1e2e7ef1db9aceb
benceBakos <bakos.bence.work@gmail.com>
2024-09-29 17:32:55 +0200

    handle todos

--- init.lua
+++ init.lua
@@ -24,14 +24,13 @@ Main.init({
 	-- rethink cmp, get snippets
 	-- 'dap', TODO
 	-- note tab open fast
-	-- treesitter advanced setup: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#incremental-selection
 	-- file managger as text buffer
 	-- handle missing binaries, optional depdendencies(in case of fuzzy)
 	-- premade macros
+	-- treesitter advanced setup: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#incremental-selection
 	-- https://www.josean.com/posts/nvim-treesitter-and-textobjects
 	--  - incremental_selection for treesitter
 	--  - textobject aware selection for treesitter
 	--
-	-- rest of the plugins
 	-- treesitter based lsp replacement? treesitter does one file, but I could map all files?
 })
