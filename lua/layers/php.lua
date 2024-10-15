bfa7076a4f4db7f7c7b3c602f6d5aed0abd4b52f
benceBakos <bakos.bence.work@gmail.com>
2024-10-15 21:03:22 +0200

    configure phpactor

--- lua/layers/php.lua
+++ lua/layers/php.lua
@@ -7,28 +7,69 @@ Php.excludeOs = {
 	Terminal.TERMUX
 }
 
-Php.dependencyBinaries = {
-	debian = { 'php', 'composer' },
-	arch = { 'php', 'composer' }
+Php.packages = {
+	'gbprod/phpactor.nvim',
+	'nvim-lua/plenary.nvim',
+	'neovim/nvim-lspconfig'
 }
 
-Php.init = function()
+Php.dependencyBinaries = {
+	debian = { 'php', 'composer', 'phpactor' },
+	arch = { 'php', 'composer', 'phpactor' }
+}
 
+Php.phpactor = nil
+
+Php.init = function()
 	-- LspConfig.intelephense.setup({
 	-- 	capabilities = capabilities,
 	-- 	init_options = {
 	-- 		licenceKey = File.get_intelephense_license()
 	-- 	},
 	-- })
-	Lsp.lspconfig.phpactor.setup({
-		init_options = {
-			["language_server_phpstan.enabled"] = true,
-			["language_server_psalm.enabled"] = true,
-			["language_server_php_cs_fixer.enabled"] = true,
-			["php_code_sniffer.enabled"] = true,
+
+	Php.phpactor = Package.want("phpactor")
+
+	if not Php.phpactor then return false end
+
+
+	Php.phpactor.setup({
+		install = {
+			path = vim.fn.stdpath("data") .. "/opt/",
+			branch = "master",
+			bin = vim.fn.stdpath("data") .. "/opt/phpactor/bin/phpactor",
 		},
-		capabilities = Lsp.capabilities
+		lspconfig = {
+			enable = true,
+			options = {
+				-- init_options = {
+				-- 	["language_server_phpstan.enabled"] = true,
+				-- 	["language_server_psalm.enabled"] = true,
+				-- 	["language_server_php_cs_fixer.enabled"] = true,
+				-- 	["php_code_sniffer.enabled"] = true,
+				-- 	["prophecy.enabled"] = true,
+				-- }
+			}
+		}
 	})
+
+	local handler = Package.want('phpactor.handler.update')
+
+	if not handler then return false end
+
+	handler()
+
+
 end
+
+Php.maps = {
+	{
+		mode = {'n','v'},
+		map = "gj",
+		to = function()
+			Php.phpactor.rpc('context_menu', {})
+		end
+	}
+}
 
 return Php
