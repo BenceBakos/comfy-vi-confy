5555ada2503ffeddf153cd754677a8dc38c265f6
Bakos Bence <bakos.bence.work@gmail.com>
2024-12-06 22:15:39 +0100

    touch basics

--- lua/layers/touch.lua
+++ lua/layers/touch.lua
@@ -4,8 +4,8 @@ File = require("utils.file")
 Touch = {}
 
 Touch.excludeOs = {
-	Terminal.DEBIAN,
-	Terminal.ARCH,
+	-- Terminal.DEBIAN,
+	-- Terminal.ARCH,
 }
 
 Touch.envCommands = {
@@ -23,7 +23,7 @@ Touch.options = {
 
 	},
 	opt = {
-		-- signcolumn = 'number'
+		mouse = 'a'
 	}
 }
 
@@ -36,8 +36,52 @@ Touch.autocmds = {
 }
 
 Touch.maps = {
-	-- {mode='n', map='th',to= ':tabfirst<CR>',options = false},
 	-- {mode='', map='', to=function() end,options=false}
+	{
+		mode = 'n',
+		map = '<ScrollWheelUp>',
+		to = function()
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, { "scroll up" })
+		end,
+		options = false
+	},
+	{
+		mode = 'n',
+		map = '<ScrollWheelDown>',
+		to = function()
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, { "scroll down" })
+		end,
+		options = false
+	},
+	{
+		mode = 'n',
+		map = '<ScrollWheelLeft>',
+		to = function()
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, { "scroll left" })
+		end,
+		options = false
+	},
+	{
+		mode = 'n',
+		map = '<ScrollWheelRight>',
+		to = function()
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, { "scroll right" })
+		end,
+		options = false
+	},
+	{
+		mode = 'n',
+		map = '<LeftMouse>',
+		to = function()
+			-- Get the mouse position
+			local mouse_pos = vim.fn.getmousepos()
+			local x = mouse_pos.wincol
+			local y = mouse_pos.winrow
+
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, { "click at"..x.."  "..y })
+		end,
+		options = { noremap = true, silent = true }
+	}
 }
 
 return Touch
