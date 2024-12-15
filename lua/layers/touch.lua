d68e8bcd027788057aab874a6d9902dee38945ef
Bakos Bence <bakos.bence.work@gmail.com>
2024-12-15 21:11:32 +0100

    fine tune touch areas

--- lua/layers/touch.lua
+++ lua/layers/touch.lua
@@ -38,7 +38,7 @@ Touch.handlers = {}
 
 Touch.handlers['<LeftRelease>'] = {
 	function(dimensions)
-		if dimensions.wincol < (dimensions.winCols * 0.95) and dimensions.winrow < (dimensions.winRows * 0.05) then
+		if dimensions.wincol < (dimensions.winCols * 0.92) and dimensions.winrow < (dimensions.winRows * 0.08) then
 			vim.cmd('q!')
 		end
 	end
@@ -46,7 +46,7 @@ Touch.handlers['<LeftRelease>'] = {
 
 Touch.handlers['<ScrollWheelUp>'] = {
 	function(dimensions)
-		if dimensions.wincol < (dimensions.winCols * 0.05) then
+		if dimensions.wincol < (dimensions.winCols * 0.08) then
 			vim.cmd('tabnext')
 		end
 	end
@@ -54,7 +54,7 @@ Touch.handlers['<ScrollWheelUp>'] = {
 
 Touch.handlers['<ScrollWheelDown>'] = {
 	function(dimensions)
-		if dimensions.wincol < (dimensions.winCols * 0.05) then
+		if dimensions.wincol < (dimensions.winCols * 0.08) then
 			vim.cmd('tabprevious')
 		end
 	end
