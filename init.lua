f53ba1b642cadf713ef481a5e0eb5f4f32c9daa9
Bakos Bence <bakos.bence.work@gmail.com>
2024-12-15 20:21:06 +0100

    remove abz from touch

--- init.lua
+++ init.lua
@@ -25,8 +25,8 @@ Main.init({
 	'macro',
 	'theme',
 	'refactor',
-	'abz',
-	-- 'touch' -- hopefully today
+	-- 'abz',
+	'touch'
 	-- where to store snippets, refactors, spells?
 	-- refactors -> treesitter based, low-code definitions
 	-- gitlab cli or gitlab nvim plugin
--- lua/layers/touch.lua
+++ lua/layers/touch.lua
@@ -3,75 +3,9 @@ File = require("utils.file")
 
 Touch = {}
 
-Touch.separator = "#--------"
-
-Touch.selectedLineSignName = "selectedLineSignName"
-Touch.storeSignGroup = "storeSignGroup"
-
-Touch.selectionCounter = 0
-
-Touch.storeBufferName = "Store"
-Touch.builderBufferName = "Builder"
-
-Touch.storeBufferId = nil
-Touch.builderBufferId = nil
-
-
 Touch.leftReleaseHandler = {}
 
 Touch.init = function()
-	Touch.initStore()
-	Touch.initBuilder()
-	Touch.initLeftReleaseHandlers()
-
-	-- define select sign
-	vim.fn.sign_define(Touch.selectedLineSignName, { text = '■', texthl = "", linehl = "", numhl = "" })
-end
-
-Touch.initBuilder = function()
-	Touch.storeBufferId = vim.api.nvim_create_buf(true, true)
-	vim.api.nvim_buf_set_name(Touch.storeBufferId, Touch.builderBufferName)
-end
-
-Touch.initStore = function()
-	-- create and focus store buffer
-	Touch.storeBufferId = vim.api.nvim_create_buf(true, true)
-	vim.api.nvim_buf_set_name(Touch.storeBufferId, Touch.storeBufferName)
-	vim.api.nvim_set_current_buf(Touch.storeBufferId)
-
-	-- read store into buffer
-	local storeContent = File.readAll(os.getenv("HOME") .. '/.config/nvim/store.sh')
-
-	if not storeContent then
-		Log.err('Failed to load store for Touch')
-		return nil
-	end
-
-
-	vim.api.nvim_buf_set_lines(Touch.storeBufferId, 0, -1, false, vim.split(storeContent, '\n'))
-end
-
-Touch.initLeftReleaseHandlers = function()
-	Touch.leftReleaseHandler['<LeftRelease>'] = {}
-	Touch.leftReleaseHandler['<LeftRelease>'][Touch.storeBufferId] = {
-		selectStoreLine = function(col,row,winId,bufferId)
-			
-		end
-		-- selectStoreSection
-		-- selectedToBuilder
-		-- selectBuilderLine
-		-- selectBuilderSection
-		-- cutBuilderSelected
-		-- copyBuilderSelected
-		-- pasteBuilderSelected
-		-- manualEdit
-		-- clearBuilder(make it a bit harder to do)
-		-- execute(validate,store,exec in new terminal session)
-		-- nextBuffer
-		-- previousBuffer
-		-- closeBuffer(only for terminals, stop execution!)
-		-- edit executed(only in terminal buffer)
-	}
 end
 
 Touch.maps = {
@@ -94,13 +28,6 @@ Touch.maps = {
 				bufferId
 			)
 
-			for _, handler in pairs(Touch.leftReleaseHandler[eventName][bufferId]) do
-				handler(
-					pointer.wincol,
-					pointer.winrow,
-					pointer.winid,
-					bufferId)
-			end
 		end,
 		options = { noremap = false, silent = true },
 	},
@@ -129,21 +56,6 @@ Touch.logEvent = function(eventName, col, row, winId, bufferId)
 		vim.api.nvim_win_get_width(winId) ..
 		" H:" .. vim.api.nvim_win_get_height(winId) .. " Win: " .. winId .. " Buf: " .. bufferId,
 	}, '')
-end
-
-Touch.insertSign = function (row)
-	Touch.selectionCounter = Touch.selectionCounter + 1
-	vim.fn.sign_place(row, Touch.storeSignGroup, Touch.selectedLineSignName, Touch.storeBufferName, {lnum = row, priority = 1})
-end
-
-Touch.removeSign = function (row)
-	Touch.selectionCounter = Touch.selectionCounter - 1
-	vim.fn.sign_unplace(Touch.storeSignGroup, { buffer = Touch.storeBufferName, id = row })
-end
-
-Touch.removeAllSign = function (bufferName)
-	Touch.selectionCounter = 0
-	vim.fn.sign_unplace(Touch.storeSignGroup, { buffer = bufferName })
 end
 
 Touch.options = {
