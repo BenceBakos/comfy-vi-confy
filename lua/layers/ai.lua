fb8244147e646382b65e46e0bc6c7d33614a1a83
Bakos Bence <bakos.bence.work@gmail.com>
2025-02-09 11:09:00 +0100

    remove avante

--- lua/layers/ai.lua
+++ lua/layers/ai.lua
@@ -9,52 +9,10 @@ Ai.excludeOs = {
 }
 
 Ai.packages = {
-	'MeanderingProgrammer/render-markdown.nvim',
-	'stevearc/dressing.nvim',
-	'MunifTanjim/nui.nvim',
-	'echasnovski/mini.icons',
-	'zbirenbaum/copilot.lua',
-	'yetone/avante.nvim',
-	'HakonHarnes/img-clip.nvim',
+
 }
 
 Ai.init = function()
-	local imgClip = Package.want('img-clip')
-
-	if not imgClip then return false end
-
-	imgClip.setup({
-		default = {
-			embed_image_as_base64 = false,
-			prompt_for_file_name = false,
-			drag_and_drop = {
-				insert_mode = true,
-			},
-		}
-	})
-
-	local copilot = Package.want('copilot')
-	if not copilot then return false end
-	copilot.setup({})
-
-	local renderMarkdown = Package.want('render-markdown')
-	if not renderMarkdown then return false end
-	renderMarkdown.setup({})
-
-	local avanteLib = Package.want('avante_lib')
-	if not avanteLib  then return false end
-	Log.log(avanteLib)
-	avanteLib.load()
-
-	-- TODO run make
-	local avante = Package.want('avante')
-	if not avante then return false end
-	avante.setup({
-		provider = 'copilot',
-		suggestion = {
-			debounce = 1000
-		}
-	})
 
 end
 
