e978c93964def933aa5339080759a0d33d2f7d2c
Bakos Bence <bakos.bence.work@gmail.com>
2025-04-01 19:42:17 +0200

    avante with cmphub

--- lua/layers/erik.lua
+++ lua/layers/erik.lua
@@ -1,4 +1,6 @@
 Terminal = require("utils.terminal")
+Package = require("utils.package")
+Os = require('os')
 
 Erik = {}
 
@@ -8,17 +10,87 @@ Erik.excludeOs = {
 
 Erik.packages = {
 	"nvim-lua/plenary.nvim",
-	"ravitemer/mcphub.nvim"
+	"ravitemer/mcphub.nvim",
+	'stevearc/dressing.nvim',
+	'MunifTanjim/nui.nvim',
+	'echasnovski/mini.icons',
+	'zbirenbaum/copilot.lua',
+	'yetone/avante.nvim',
+	'HakonHarnes/img-clip.nvim',
 }
 
 Erik.mcphub = nil
 
 Erik.init = function()
+	Erik.initMcphub()
+	Erik.initAvante()
+end
+
+Erik.initMcphub = function()
 	Erik.mcphub = Package.want("mcphub")
 
 	if not Erik.mcphub then return false end
 
-	Erik.mcphub.setup()
+	Erik.mcphub.setup({
+		extensions = {
+			avante = {
+				auto_approve_mcp_tool_calls = true, -- Auto approves mcp tool calls.
+			}
+		}
+	})
+end
+
+Erik.initAvante = function()
+	local imgClip = Package.want('img-clip')
+
+	if not imgClip then return false end
+
+	imgClip.setup({
+		default = {
+			embed_image_as_base64 = false,
+			prompt_for_file_name = false,
+			drag_and_drop = {
+				insert_mode = true,
+			},
+		}
+	})
+
+	local copilot = Package.want('copilot')
+	if not copilot then return false end
+	copilot.setup({})
+
+	local avanteLib = Package.want('avante_lib')
+	if not avanteLib then return false end
+	avanteLib.load()
+
+	-- TODO run make
+	local avante = Package.want('avante')
+	if not avante then return false end
+	avante.setup({
+		provider = 'copilot',
+		auto_suggestions_provider = "copilot",
+		claude = {
+			endpoint = "https://api.anthropic.com",
+			model = "claude-3-7-sonnet-20241022",
+			temperature = 0,
+			max_tokens = 4096,
+		},
+		behaviour = {
+			auto_suggestions = false, -- Experimental stage
+		},
+		-- other config
+		-- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
+		system_prompt = function()
+			local hub = require("mcphub").get_hub_instance()
+			return hub:get_active_servers_prompt()
+		end,
+		-- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+		custom_tools = function()
+			return {
+				Package.want("mcphub.extensions.avante").mcp_tool(),
+			}
+		end,
+	})
 end
 
 return Erik
